519b59c04465b517dcd5663c3e05ce93b630f855
Li Fei <hefrankeleyn@gamil.com>
2021-05-31 21:57:42 +0800

    单词搜索

--- LeetCodeWP/src/main/java/com/hef/week08/homework/Leet212WordSearchII.java
+++ LeetCodeWP/src/main/java/com/hef/week08/homework/Leet212WordSearchII.java
@@ -10,19 +10,29 @@ public class Leet212WordSearchII {
 
 
     public static void main(String[] args) {
-        System.out.println('z'-'a');
-        char[][] board = {
+        /*char[][] board = {
                 {'o','a','a','n'},
                 {'e','t','a','e'},
                 {'i','h','k','r'},
                 {'i','f','l','v'}};
+        String[] words = {"oath","pea","eat","rain"};*/
 
-        String[] words = {"oath","pea","eat","rain"};
+        /*char[][] board ={
+                {'o','a','b','n'},
+                {'o','t','a','e'},
+                {'a','h','k','r'},
+                {'a','f','l','v'}
+        };
+
+        String[] words = {"oa",  "oaa"};*/
+
+        char[][] board = {{'a','b'}};
+        String[] words = {"ab"};
 
         Leet212WordSearchII wordSearchII = new Leet212WordSearchII();
+
         List<String> result = wordSearchII.findWords(board, words);
         System.out.println(result);
-
 
     }
 
@@ -34,13 +44,15 @@ public class Leet212WordSearchII {
         List<String> result = new ArrayList<>();
 
         Trie trie = new Trie();
+
         for (String w: words) {
             trie.insert(w);
         }
+
         Set<String> resSet = new HashSet<>();
         for (int i=0; i<board.length; i++) {
             for (int j=0; j<board[i].length; j++) {
-                dfs(i, j, board, "", trie, resSet);
+                dfs(i, j, board, new StringBuilder(), trie, resSet);
             }
         }
         result.addAll(resSet);
@@ -49,23 +61,26 @@ public class Leet212WordSearchII {
         return result;
     }
 
-    private void dfs(int i, int j, char[][] board, String res, Trie trie, Set<String> resSet) {
-        if (i<0 || i>=board.length || j<0 || j>board.length) {
-            return;
-        }
-
-
-        if (trie.searchWord(res+board[i][j])) {
-            resSet.add(res+board[i][j]);
-            return;
+    private void dfs(int i, int j, char[][] board, StringBuilder sb, Trie trie, Set<String> resSet) {
+        sb.append(board[i][j]);
+        char c = board[i][j];
+        int n = sb.length();
+        board[i][j] = '@';
+        if (trie.searchWord(sb.toString())) {
+            resSet.add(sb.toString());
         }
 
         for (int x=0; x<dx.length; x++) {
             int x1 = i + dx[x];
-            int y1 = i + dy[x];
-            dfs(x1, y1, board, res, trie, resSet);
+            int y1 = j + dy[x];
+            if (x1>=0 && x1<board.length && y1>=0 && y1<board[x1].length && board[x1][y1]!='@') {
+                if (trie.isPrefix(sb.toString()+board[x1][y1])) {
+                    dfs(x1, y1, board, sb, trie, resSet);
+                }
+            }
         }
-
+        board[i][j] = c;
+        sb.deleteCharAt(n-1);
     }
 
     class Trie {
@@ -74,7 +89,7 @@ public class Leet212WordSearchII {
 
         public Trie() {
             this.children = new Trie[26];
-            this.isEnd = true;
+            this.isEnd = false;
         }
 
         public void insert(String word) {
@@ -84,9 +99,9 @@ public class Leet212WordSearchII {
                 if (node.children[index]==null) {
                     node.children[index] = new Trie();
                 }
-                node.isEnd = false;
                 node = node.children[index];
             }
+            node.isEnd = true;
         }
 
         public boolean isPrefix(String str) {
