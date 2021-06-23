8f318ab64cf840d00f0199df09b097633ce810ad
Li Fei <hefrankeleyn@gamil.com>
2021-06-23 21:35:53 +0800

    java版 BloomFilter

---
+++ LeetCodeWP/src/main/java/com/hef/week09/BloomFilter.java
@@ -0,0 +1,69 @@
+package com.hef.week09;
+
+import java.util.BitSet;
+
+/**
+ * @Date 2021/6/23
+ * @Author lifei
+ */
+public class BloomFilter {
+
+    private static final int DEFAULT_SIZE = 2 << 24;
+    private static final int[] seeds = new int[]{5, 7, 11, 13, 31, 37, 61};
+    private BitSet bits = new BitSet(DEFAULT_SIZE);
+
+    private SimpleHash[] func = new SimpleHash[seeds.length];
+
+    public BloomFilter() {
+        for (int i = 0; i < seeds.length; i++) {
+            func[i] = new SimpleHash(DEFAULT_SIZE, seeds[i]);
+        }
+    }
+
+    public void add(String value) {
+        for (SimpleHash f : func) {
+            bits.set(f.hash(value), true);
+        }
+    }
+
+    public boolean contains(String value) {
+        if (value==null) {
+            return false;
+        }
+        boolean ret = true;
+        for (SimpleHash simpleHash : func) {
+            ret = ret & bits.get(simpleHash.hash(value));
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        BloomFilter bloomFilter = new BloomFilter();
+        String oneStr = "helloword";
+        bloomFilter.add(oneStr);
+        System.out.println(bloomFilter.contains(oneStr));
+        System.out.println(bloomFilter.contains("aaa"));
+    }
+
+
+    public static class SimpleHash{
+        private int cap;
+        private int seed;
+
+        public SimpleHash(int cap, int seed) {
+            this.cap = cap;
+            this.seed = seed;
+        }
+
+        public int hash(String value) {
+            int result = 0;
+            int len = value.length();
+            for (int i = 0; i < len; i++) {
+                result = seed * result + value.charAt(i);
+            }
+            return (cap-1) & result;
+        }
+    }
+}
+
+
