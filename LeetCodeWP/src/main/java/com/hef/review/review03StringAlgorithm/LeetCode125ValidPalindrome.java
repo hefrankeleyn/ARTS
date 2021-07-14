332f8789e6986fd13f859663ddda85c240daae2c
Li Fei <hefrankeleyn@gamil.com>
2021-07-14 22:15:32 +0800

    验证回文串

---
+++ LeetCodeWP/src/main/java/com/hef/review/review03StringAlgorithm/LeetCode125ValidPalindrome.java
@@ -0,0 +1,23 @@
+package com.hef.review.review03StringAlgorithm;
+
+/**
+ * @Date 2021/7/14
+ * @Author lifei
+ */
+public class LeetCode125ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+        if (s==null || s.length()==0) return true;
+        int i = 0, j = s.length()-1;
+        String ls = s.toLowerCase();
+        while (i<=j) {
+            while(i<=j && !((ls.charAt(i)>='a' && ls.charAt(i)<='z')||(ls.charAt(i)>='0' && ls.charAt(i)<='9'))) i++;
+            while(i<=j && !((ls.charAt(j)>='a' && ls.charAt(j)<='z')||(ls.charAt(j)>='0' && ls.charAt(j)<='9'))) j--;
+            if (i<=j && ls.charAt(i)!=ls.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+}
